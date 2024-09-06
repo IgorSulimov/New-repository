669b6a8a9f7508fbf6c88905d55046b114c99660
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-09-06 21:13:16 +0300

    Update main.c

--- Magic_Square_Matrix/main.c
+++ Magic_Square_Matrix/main.c
@@ -4,24 +4,38 @@
 #include <stdbool.h>
 int main()
 {
-	//matrix values ??and its size: 3|| 4 9 2 3 5 7 8 1 6
-	bool t;
 	char* name = "d:/1.txt";
+	int elements;
 	int size;
 	FILE* f1;
-	fopen_s(&f1, "d:/1.txt", "r");
-	if (!f1)
-		return -1;
-	fscanf_s(f1, "%d", &size);
+	fopen_s(&f1, name, "w");
+	printf("Print SIZE\n");
+	scanf_s("%d", &size);
+	fprintf(f1, "%d\n", size);
+	int tmp_size = size * size;
+	printf("Print %d Elements\n", size * size);
+	for (; tmp_size > 0; tmp_size--)
+	{
+		scanf_s("%d", &elements);
+		fprintf(f1, "%d\n", elements);
+	}
+
+	fclose(f1);
 
 	int** matrix = calloc(size, sizeof(int));
 	for (unsigned i = 0; i < size; i++)
 		matrix[i] = calloc(size, sizeof(int));
-	fclose(f1);
 
 
 	Creat_Matrix(name, matrix);
+	printf("\nMatrix:\n\n");
 	Print_Matrix(matrix, size);
-	printf("\n\n\n");
-	printf("%d",Magick_Matrix(matrix, size));
+	if (("%d", Magick_Matrix(matrix, size)))
+	{
+		printf("\nMagick Square: True\n");
+	}
+	else
+		printf("\nMagick Square: False\n");
+
+//matrix values and its size: 3|| 4 9 2 3 5 7 8 1 6
 }
