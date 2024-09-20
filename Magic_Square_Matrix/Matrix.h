28597c88d1c90215972805044089075b3ae4daa5
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-09-21 00:02:50 +0300

    Update Matrix.h

--- Magic_Square_Matrix/Matrix.h
+++ Magic_Square_Matrix/Matrix.h
@@ -4,3 +4,7 @@ int Creat_Matrix(char* name_file, int** matrix);
 void Print_Matrix(int** matrix, size_t size_matrix);
 
 bool Magick_Matrix(int** matrix, int size);
+
+int Write_File(char* name_file, int size);
+
+int Read_File(char* name_file, int size);
