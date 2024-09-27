296b0a28ea1aecf1ad3c0fc0c46a174d152ec214
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-09-28 00:51:17 +0300

    Update SinglList.cpp

--- Singl_List_F/SinglList.cpp
+++ Singl_List_F/SinglList.cpp
@@ -19,72 +19,6 @@ SList* creat_node(char* data_)
 	return element;
 }
 
-void Push_Top(SList** List, char* data_)
-{
-	SList* element = creat_node();
-	strcpy(element->data, data_);
-
-	if ((*List)->size == 0)
-	{
-		element->size = (*List)->size;
-		*List = element;
-	}
-	else
-	{
-		element->size = (*List)->size;
-		element->next = *List;
-		*List = element;
-	}
-	(*List)->size++;
-}
-
-void Push_Back(SList** List, char* data_)
-{
-	SList* element = creat_node(data_);
-	if ((*List)->size > 0)
-	{
-		SList* tmp = *List;
-		while ((tmp)->next != NULL)
-		{
-			tmp = (tmp)->next;
-		}
-		(tmp)->next = element;
-	}
-	else
-	{
-		element->size = (*List)->size;
-		(*List) = element;
-	}
-	(*List)->size++;
-}
-
-void Push_Place(SList** List, char* data_, int place,int rep)
-{
-	SList* element = creat_node();
-	strcpy(element->data, data_);
-	element->repeat = rep;
-	SList* tmp = *List;
-	SList* prev = creat_node();
-	int fl = 1;
-	if (fl == 1) {
-		if (place == 0)
-		{
-			Push_Top(List, data_);
-		}
-		else
-		{
-			prev->next = tmp;
-			for (; place; place--)
-			{
-				tmp = tmp->next;
-				prev = prev->next;
-			}
-			prev->next = element;
-			element->next = tmp;
-			(*List)->size++;
-		}
-	}
-}
 char* Pop_Back(SList** List)
 {
 	char* delet_element;
@@ -136,41 +70,194 @@ char* Pop_Top(SList** List)
 	return delet_element;
 }
 
-
-void Print_List(SList** List)
+void Pop_Word(SList** List, char* data_)
 {
 	SList* tmp = *List;
-	if (List != NULL)
+	SList* prev_tmp = creat_node();
+	prev_tmp->next = tmp;
+	while (strcmp(tmp->data,data_) != 0)
 	{
-		if (tmp->data != NULL)
+		tmp = tmp->next;
+		prev_tmp = prev_tmp->next;
+	}
+	prev_tmp->next = tmp->next;
+	free(tmp);
+	prev_tmp = prev_tmp->next;
+	if (prev_tmp)
+	{
+		while (strcmp(prev_tmp->data, data_) == 0)
 		{
-			while (tmp != NULL)
-			{
-				printf("%s %d\n", tmp->data,tmp->repeat);
-				tmp = tmp->next;
-			}
+			prev_tmp->repeat--;
+			prev_tmp = prev_tmp->next;
 		}
+	}
+}
+
+void Push_Top(SList** List, char* data_)
+{
+	SList* element = creat_node();
+	strcpy(element->data, data_);
+
+	if ((*List)->size == 0)
+	{
+		element->size = (*List)->size;
+		*List = element;
 	}
 	else
 	{
-		printf("list is empty");
+		element->size = (*List)->size;
+		element->next = *List;
+		*List = element;
 	}
+	(*List)->size++;
 }
 
-void Toshka(SList** List)
+void Push_Back(SList** List, char* data_)
+{
+	SList* element = creat_node(data_);
+	if ((*List)->size > 0)
+	{
+		SList* tmp = *List;
+		while ((tmp)->next != NULL)
+		{
+			tmp = (tmp)->next;
+		}
+		(tmp)->next = element;
+	}
+	else
+	{
+		element->size = (*List)->size;
+		(*List) = element;
+	}
+	(*List)->size++;
+}
+
+void Push_Word(SList** List, char* data_)
 {
 	SList* tmp = *List;
-	while (tmp->next != NULL)
+	int place = 0;
+	int rep = 1;
+	int fl = 1;
+	while (fl != 0)
 	{
-		tmp = tmp->next;
+		if (strcmp(tmp->data, data_) == 0 && fl == 1)
+		{
+			while (strcmp(tmp->data, data_) == 0 && tmp->next != NULL)
+			{
+				place++;
+				rep++;
+				tmp = tmp->next;
+			}
+			if (tmp->next == NULL)
+			{
+				if (strcmp(tmp->data, data_) == -1 && fl == 1)
+				{
+					place++;
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+				if (strcmp(tmp->data, data_) == 1 && fl == 1)
+				{
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+				if (strcmp(tmp->data, data_) == 0 && fl == 1)
+				{
+					place++;
+					rep++;
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+			}
+			if (fl == 1)
+			{
+				Push_Place(List, data_, place, rep);
+				fl = 0;
+			}
+		}
+
+
+		if (strcmp(tmp->data, data_) == -1 && fl == 1)
+		{
+			while (strcmp(tmp->data, data_) == -1 && tmp->next != NULL)
+			{
+				place++;
+				tmp = tmp->next;
+			}
+			if (tmp->next == NULL)
+			{
+				if (strcmp(tmp->data, data_) == -1 && fl == 1)
+				{
+					place++;
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+				if (strcmp(tmp->data, data_) == 1 && fl == 1)
+				{
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+				if (strcmp(tmp->data, data_) == 0 && fl == 1)
+				{
+					place++;
+					rep++;
+					Push_Place(List, data_, place, rep);
+					fl = 0;
+				}
+			}
+		}
+
+
+		if (strcmp(tmp->data, data_) == 1 && fl == 1)
+		{
+			Push_Place(List, data_, place, rep);
+			fl = 0;
+		}
 	}
-	char toshka[100] = ".";
-	strcat(tmp->data,toshka);
 }
 
-SList* Push_F(SList* List, char* name_file)
+void Push_Place(SList** List, char* data_, int place,int rep)
+{
+	SList* element = creat_node();
+	strcpy(element->data, data_);
+	element->repeat = rep;
+	SList* tmp = *List;
+	SList* prev = creat_node();
+	int fl = 1;
+	if (fl == 1) {
+		if (place == 0)
+		{
+			Push_Top(List, data_);
+		}
+		else
+		{
+			prev->next = tmp;
+			for (; place; place--)
+			{
+				tmp = tmp->next;
+				prev = prev->next;
+			}
+			prev->next = element;
+			element->next = tmp;
+			(*List)->size++;
+		}
+	}
+}
+
+void Print_List(SList* List)
+{
+	while (List->next != NULL)
+	{
+		printf("%s %d\n", List->data,List->repeat);
+		List = List->next;
+	}
+	printf("%s.%d\n", List->data, List->repeat);
+}
+
+SList* Order_F(char* name_file)
  {
 	char word[200];
+	SList* List = creat_node();
 	FILE* f;
 	fopen_s(&f, name_file, "r");
 	if (!f)
@@ -178,95 +265,13 @@ SList* Push_F(SList* List, char* name_file)
 	while (fscanf(f, "%s", word) != -1)
 	{
 		SList* tmp = List;
-		int place = 0;
-		int rep = 1;
-		int fl = 1;
 		if ((List)->size == 0)
 		{
 			Push_Top(&List, word);
-			fl = 0;
 		}
-			while (fl == 1)
-			{
-
-
-				if (strcmp(tmp->data, word) == 0 && fl == 1)
-				{
-					while (strcmp(tmp->data, word) == 0 && tmp->next != NULL)
-					{
-						place++;
-						rep++;
-						tmp = tmp->next;
-					}
-					if (tmp->next == NULL)
-					{
-						if (strcmp(tmp->data, word) == -1 && fl == 1)
-						{
-							place++;
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-						if (strcmp(tmp->data, word) == 1 && fl == 1)
-						{
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-						if (strcmp(tmp->data, word) == 0 && fl == 1)
-						{
-							place++;
-							rep++;
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-					}
-					if (fl == 1) 
-					{
-						Push_Place(&List, word, place, rep);
-						fl = 0;
-					}
-				}
-
-
-				if (strcmp(tmp->data, word) == -1 && fl == 1)
-				{
-					while (strcmp(tmp->data, word) == -1 && tmp->next != NULL)
-					{
-						place++;
-						tmp = tmp->next;
-					}
-					if (tmp->next == NULL)
-					{
-						if (strcmp(tmp->data, word) == -1 && fl == 1)
-						{
-							place++;
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-						if (strcmp(tmp->data, word) == 1 && fl == 1)
-						{
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-						if(strcmp(tmp->data,word) == 0 && fl == 1)
-						{
-							place++;
-							rep++;
-							Push_Place(&List, word, place, rep);
-							fl = 0;
-						}
-					}
-				}
-
-
-				if (strcmp(tmp->data, word) == 1 && fl == 1)
-				{
-					Push_Place(&List, word, place,rep);
-					fl = 0;
-				}
-			}
-			if (fl == 1)
-				Push_Place(&List, word, place,rep);
+		else
+			Push_Word(&List, word);
 	}
-	Toshka(&List);
 	return List;
 }
+
