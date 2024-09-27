1d0556645798cf9614771424ca2665c62436cae0
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-09-28 00:50:31 +0300

    Update main.cpp

--- Singl_List_F/main.cpp
+++ Singl_List_F/main.cpp
@@ -3,13 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 #pragma warning(disable:4996)
+// Дана Непустая последовательность слов, разделенных одним и более пробелом, за последним словом стоит точка.
+// Длина слова не превышает m символов.
+// Описать функцию которая организует слова в упорядоченный по алфавиту список 
+// при этом для каждого слова указывается число его вхождений в эту последовательность
 int main()
 {
 	char data[] = "d:/Word.txt";
-	SList* list = creat_node();
-	list = Push_F(list, data);
-	Print_List(&list);
-	Pop_Top(&list);
-	printf("\n\n");
-	Print_List(&list);
+	SList* list;
+	list = Order_F(data);
+	Print_List(list);
 }
