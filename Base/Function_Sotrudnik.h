1f2c6adc40d06ff8069ab473d4e5274e7fcb8486
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-10-12 01:09:45 +0300

    Delete Base directory

--- Base/Function_Sotrudnik.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include "Sotrudnik.h"
-
-int Sozd_F(char* file_name, int& k, Sotrudnik* a);
-float FirstRec(Sotrudnik* arr_, int size, int& k, char** Fio_25, int year_);
