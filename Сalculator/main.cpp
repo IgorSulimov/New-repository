07e0f4b0ae55cd21cc328df38650e51a6ac80e53
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-11-16 07:56:29 +0300

    Update main.cpp

--- Сalculator/main.cpp
+++ Сalculator/main.cpp
@@ -1,7 +1,17 @@
 #include "Function.hpp"
-
+#include "Menu.hpp"
 int main()
 {
-
+    int size = 0;
+    Line line;
+    Function* selected;
+    Exp e;
+    Function* arr[] = { &line,&e };
+    size = sizeof(arr) / sizeof(Function*);
+    Menu menu(arr, size);
+    while (selected = menu.Select())
+    {
+        selected->Calculate();
+    }
     return 0;
 }
