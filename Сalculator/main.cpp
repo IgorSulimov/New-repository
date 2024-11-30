da38133ed4ce465ff4558d86d60e892b8b05cf7d
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-11-30 10:04:47 +0300

    Update main.cpp

--- Сalculator/main.cpp
+++ Сalculator/main.cpp
@@ -2,13 +2,16 @@
 #include "Menu.hpp"
 int main()
 {
+    setlocale(LC_ALL, ".1251");
     int size = 0;
     Line line;
     Function* selected;
     Exp e;
-    Function* arr[] = { &line,&e };
+    pow_3 p;
+    Function* arr[] = { &line,&e,&p };
     size = sizeof(arr) / sizeof(Function*);
     Menu menu(arr, size);
+
     while (selected = menu.Select())
     {
         selected->Calculate();
