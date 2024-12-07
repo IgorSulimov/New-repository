79262ee426d0e9829249cd1a6f2dcc61212fa1cc
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-12-07 11:10:57 +0300

    Update main.cpp

--- Matrix_ALL/main.cpp
+++ Matrix_ALL/main.cpp
@@ -6,31 +6,43 @@
 int main() {
     setlocale(LC_ALL, "RUS");
     SquareMatrix a(3);
-    printf("Введите матрицу а:\n");
-    Vector vect;
-    a.Input();
-    SquareMatrix a(2);
     cin >> a;
     cout << a;
-    Vector bb(4);
-    bb = tracematrix(a, 4);
-    cout << bb;
-    //Matrix b(2,2);
-    //cin >> b;
-    //cout << b;
-    //printf("Sum\n");
-    //Matrix sum = a + b;
-    //cout << sum;
-    //printf("Sub\n");
-    //Matrix sub = a - b;
-    //cout << sub;
-    //printf("Mult\n");
-    //Matrix mult = a * b;
-    //cout << mult;
-    //printf("Транеспонирование матрицы а");
-    //Matrix Tr = a.Transpose();
-    //cout << Tr;
-    return 0;
-}
+    SquareMatrix b(3);
+    cin >> b;
+    cout << b;
+    Matrix c = a + b;
+    printf("\nCумма матриц:\n");
+    cout << c;
+    printf("\n\n");
+    Matrix d = a * b;
+    printf("\nПроизведение матриц:\n");
+    cout << d;
+    printf("\n\n");
+    Matrix e = a - b;
+    printf("\nРазность матриц:\n");
+    cout << e;
+    printf("\n\n");
+    SquareMatrix g = a;
+    g.Transpon();
+    printf("Транспонирование матрицы\n");
+    cout << g;
+    printf("\n\n");
+    SquareMatrix h = a^3;
+    printf("Матрица в 3 степени\n");
+    cout << h;
+    printf("\n\n");
 
+    printf("Максимальные диагонали\n");
+    Vector bb;
+    bb = Maxfromdiag(a);
+    cout << bb;
+    printf("\n\n");
+
+    printf("След\n");
+    Vector cc(3);
+    cc = tracematrix(a, 3);
+    cout << cc;
+
+    return 0;
 
