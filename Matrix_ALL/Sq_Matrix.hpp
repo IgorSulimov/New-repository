ade6e21d4f349022f1dbf553f2ae558b0d7fc3ae
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2024-12-19 09:27:46 +0300

    Update Sq_Matrix.hpp

--- Matrix_ALL/Sq_Matrix.hpp
+++ Matrix_ALL/Sq_Matrix.hpp
@@ -1,3 +1,4 @@
+
 #pragma once
 #include "Matrix.hpp" 
 #include "Vector.hpp"
@@ -14,7 +15,15 @@ public:
     double trace();
     SquareMatrix& Arithmetic_mean();
     double Arif(SquareMatrix& mat, int r, int c);
+    SquareMatrix& operator+=(const SquareMatrix& other_);
+    SquareMatrix Pow11(int power_);
+    friend ostream& operator <<(ostream& stream, SquareMatrix& mat);
+    friend istream& operator >>(istream& in_, SquareMatrix& mat);
+    friend ifstream& operator>>(ifstream& in_, SquareMatrix& mat);
 
 };
 Vector Maxfromdiag(SquareMatrix matrix);
 Vector tracematrix(SquareMatrix& matrix, int n);
+ostream& operator <<(ostream& stream, SquareMatrix& mat);
+istream& operator >>(istream& in_, SquareMatrix& mat);
+ifstream& operator>>(ifstream& in_, SquareMatrix& mat);
