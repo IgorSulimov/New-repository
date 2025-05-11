0ecb5512e0c8c510abdd84207f1873f8e81d97d6
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2025-05-11 23:41:08 +0300

    Update main.cpp

--- Lexical_Analyzator_EVM/main.cpp
+++ Lexical_Analyzator_EVM/main.cpp
@@ -6,26 +6,28 @@
 #include <vector>
 #include "Hash.hpp"
 int BSE = 0;
+int Count_Line_TL = 0;
+int Count_TL = 1;
 class Analyzator
 {
 private:
 	string str;
-	typedef void(Analyzator::* pfuncType)();// указатель на функцию состояния
+	typedef void(Analyzator::* pfuncType)();// СѓРєР°Р·Р°С‚РµР»СЊ РЅР° С„СѓРЅРєС†РёСЋ СЃРѕСЃС‚РѕСЏРЅРёСЏ
 	pfuncType pfunc;
-	int RCH, // регистр числа
-		RZ, // регистр знака
-		RP, // регистр порядка
-		RS, // регистр счётчика                                                                      
-		RKL, // регистр класса лексем
-		ROT, // регистр значения отношения
-		RZN, // регистр значения символа
-		RSOS, // регистр состояния 
-		ROB, // регистр обнаружения
-		RK, // регистр значения класса символа 
-		RSTR, // регистр строка
-		RI, // регистр номера буквы в имени переменной
-		RSE; // регистр счетчика ошибок
-	vector<int> InitialVector; // начальный вектор
+	int RCH, // СЂРµРіРёСЃС‚СЂ С‡РёСЃР»Р°
+		RZ, // СЂРµРіРёСЃС‚СЂ Р·РЅР°РєР°
+		RP, // СЂРµРіРёСЃС‚СЂ РїРѕСЂСЏРґРєР°
+		RS, // СЂРµРіРёСЃС‚СЂ СЃС‡С‘С‚С‡РёРєР°                                                                      
+		RKL, // СЂРµРіРёСЃС‚СЂ РєР»Р°СЃСЃР° Р»РµРєСЃРµРј
+		ROT, // СЂРµРіРёСЃС‚СЂ Р·РЅР°С‡РµРЅРёСЏ РѕС‚РЅРѕС€РµРЅРёСЏ
+		RZN, // СЂРµРіРёСЃС‚СЂ Р·РЅР°С‡РµРЅРёСЏ СЃРёРјРІРѕР»Р°
+		RSOS, // СЂРµРіРёСЃС‚СЂ СЃРѕСЃС‚РѕСЏРЅРёСЏ 
+		ROB, // СЂРµРіРёСЃС‚СЂ РѕР±РЅР°СЂСѓР¶РµРЅРёСЏ
+		RK, // СЂРµРіРёСЃС‚СЂ Р·РЅР°С‡РµРЅРёСЏ РєР»Р°СЃСЃР° СЃРёРјРІРѕР»Р° 
+		RSTR, // СЂРµРіРёСЃС‚СЂ СЃС‚СЂРѕРєР°
+		RI, // СЂРµРіРёСЃС‚СЂ РЅРѕРјРµСЂР° Р±СѓРєРІС‹ РІ РёРјРµРЅРё РїРµСЂРµРјРµРЅРЅРѕР№
+		RSE; // СЂРµРіРёСЃС‚СЂ СЃС‡РµС‚С‡РёРєР° РѕС€РёР±РѕРє
+	vector<int> InitialVector; // РЅР°С‡Р°Р»СЊРЅС‹Р№ РІРµРєС‚РѕСЂ
 	enum RK
 	{
 		Letter, Digit, Ar_Oper, Relationship, Left_Bracket, Right_Bracket, Point, Space, End_Line, End_File
@@ -60,7 +62,10 @@ private:
 public:
 	Analyzator();
 	void Start(const char* filename);
-
+	void Print_TL();
+	string String_View_TL(int i);
+	void Print_TO();
+	void Print_TS();
 	struct Str
 	{
 		int RowLabl;
@@ -87,7 +92,7 @@ public:
 		int alternativa;
 		void(Analyzator:: *pfunc)();
 	};
-	vector<node> Table; // таблица распознавания
+	vector<node> Table; // С‚Р°Р±Р»РёС†Р° СЂР°СЃРїРѕР·РЅР°РІР°РЅРёСЏ
 
 	void next()
 	{
@@ -288,8 +293,7 @@ public:
 	}
 	void B1d()
 	{
-		CreatToken();
-		ROB = InitialVector[RZN - 1];
+		ROB++;
 		pfunc = &Analyzator::B1;
 	}
 	void B1e()
@@ -568,6 +572,7 @@ Analyzator::Analyzator() : InitialVector(26), Table(26), TL(512), TS(512)
 	NTO = 287;
 	RI = RCH = RP = RS = RSE = 0;
 
+
 	InitialVector[0] = 0;
 	InitialVector[1] = 0;
 	InitialVector[2] = 0;
@@ -603,37 +608,53 @@ Analyzator::Analyzator() : InitialVector(26), Table(26), TL(512), TS(512)
 	Table[5].alternativa = 8;
 	Table[17].alternativa = 22;
 	Table[0].letter = 14;
+	Table[0].pfunc = &Analyzator::B1d;
 	Table[1].letter = 4;
 	Table[1].pfunc = &Analyzator::A2q;
 	Table[2].letter = 15;
+	Table[2].pfunc = &Analyzator::B1d;
 	Table[3].letter = 18;
 	Table[3].pfunc = &Analyzator::F1b;
 	Table[4].letter = 15;
+	Table[4].pfunc = &Analyzator::B1d;
 	Table[5].letter = 20;
+	Table[5].pfunc = &Analyzator::B1d;
 	Table[6].letter = 15;
 	Table[6].pfunc = &Analyzator::E1a;
 	Table[7].letter = 19;
+	Table[7].pfunc = &Analyzator::B1d;
 	Table[8].letter = 21;
+	Table[8].pfunc = &Analyzator::B1d;
 	Table[9].letter = 2;
 	Table[9].pfunc = &Analyzator::E1b;
 	Table[10].letter = 6;
 	Table[10].pfunc = &Analyzator::A2r;
 	Table[11].letter = 5;
+	Table[11].pfunc = &Analyzator::B1d;
 	Table[12].letter = 20;
+	Table[12].pfunc = &Analyzator::F1a;
 	Table[13].letter = 5;
+	Table[13].pfunc = &Analyzator::B1d;
 	Table[14].letter = 24;
+	Table[14].pfunc = &Analyzator::B1d;
 	Table[15].letter = 20;
 	Table[15].pfunc = &Analyzator::C1a;
 	Table[16].letter = 5;
+	Table[16].pfunc = &Analyzator::B1d;
 	Table[17].letter = 20;
+	Table[17].pfunc = &Analyzator::B1d;
 	Table[18].letter = 21;
+	Table[18].pfunc = &Analyzator::B1d;
 	Table[19].letter = 18;
+	Table[19].pfunc = &Analyzator::B1d;
 	Table[20].letter = 14;
 	Table[20].pfunc = &Analyzator::A2s;
 	Table[21].letter = 13;
 	Table[21].pfunc = &Analyzator::G1a;
 	Table[22].letter = 20;
+	Table[22].pfunc = &Analyzator::B1d;
 	Table[23].letter = 5;
+	Table[23].pfunc = &Analyzator::B1d;
 	Table[24].letter = 16;
 	Table[24].pfunc = &Analyzator::A2t;
 	Table[25].letter = 15;
@@ -644,17 +665,91 @@ void Analyzator::Start(const char* filename)
 	ifstream in(filename);
 	if (!in)
 	{
-		cout << "Не удалось открыть файл";
+		cout << "РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р»";
 		return;
 	}
 	while (!in.eof())
 	{
 		getline(in, str);
 		str += '\n';
+		Count_Line_TL++;
 		Parse();
 
 	}
+	cout << '\n';
+	cout << "TL" << endl;
+	Print_TL();
+	cout << '\n','\n','\n';
+	cout << "TO:" << endl;
+	Print_TO();
+	cout << '\n', '\n', '\n';
+	cout << "TS:";
+	Print_TS();
 	in.close();
+}
+void Analyzator::Print_TL()
+{
+	for (int i = 1; i < Count_TL; i++)
+	{
+		cout << TL[i].TokenClass << "||" << TL[i].TokenValue << "||" << String_View_TL(i) << endl;
+	}
+}
+string Analyzator::String_View_TL(int i)
+{
+	string str = "";
+	if (TL[i].TokenClass == 1)
+		str = "РњРµС‚РєР°";
+	if (TL[i].TokenClass == 2)
+		str = "РћРїРµСЂР°РЅРґ";
+	if (TL[i].TokenClass == 3)
+		str = "РђСЂРёС„РјРµС‚РёС‡РµСЃРєР°СЏ РѕРїРµСЂР°С†РёСЏ";
+	if (TL[i].TokenClass == 4)
+		str = "РћРїРµСЂР°С†РёСЏ РѕС‚РЅРѕС€РµРЅРёСЏ";
+	if (TL[i].TokenClass == 5)
+		str = "NEXT";
+	if (TL[i].TokenClass == 6)
+		str = "LET";
+	if (TL[i].TokenClass == 7)
+		str = "FOR";
+	if (TL[i].TokenClass == 8)
+		str = "GOTO";
+	if (TL[i].TokenClass == 9)
+		str = "GOSUB";
+	if (TL[i].TokenClass == 10)
+		str = "(";
+	if (TL[i].TokenClass == 11)
+		str = ")";
+	if (TL[i].TokenClass == 12)
+		str = "IF";
+	if (TL[i].TokenClass == 13)
+		str = "RETURN";
+	if (TL[i].TokenClass == 14)
+		str = "END";
+	if (TL[i].TokenClass == 15)
+		str = "TO";
+	if (TL[i].TokenClass == 16)
+		str = "STEP";
+	if (TL[i].TokenClass == 17)
+		str = "REM";
+	if (TL[i].TokenClass == 18)
+		str = "РћС€РёР±РєР°";
+	if (TL[i].TokenClass == 19)
+		str = "РљРѕРЅРµС† С„Р°Р№Р»Р°";
+	return str;
+}
+void Analyzator::Print_TO()
+{
+	for (int i = 0; i < 1024; i++)
+	{
+		if (TO[i] != 0)
+		{
+			cout << i << "||" << TO[i] << endl;
+		}
+	}
+}
+void Analyzator::Print_TS()
+{
+	TS.print();
 }
 void Analyzator::Parse()
 {
@@ -718,11 +813,11 @@ void Analyzator::Transliterator(char ch)
 	if (ch == '<' || ch == '>' || ch == '=')
 	{
 		RK = Relationship;
-		if (ch == '>')
+		if (ch == '=')
 			RZN = 1;
 		if (ch == '<')
 			RZN = 2;
-		if (ch == '=')
+		if (ch == '>')
 			RZN = 3;
 	}
 }
@@ -734,9 +829,9 @@ void Analyzator::CreatToken()
 	if (RKL == ROWLABEL || RKL == GOTO || RKL == GOSUB)
 	{
 		bool fl = true;
-		for (int i = 0; i < TS.Get_NTS(); i++)
+		for (int i = 0; i < 1; i++)// TS.Get_NTS();
 		{
-			if (TS.find(RSTR))
+			if (TS.Find(RSTR))
 			{
 				t.TokenValue = i;
 				fl = false;
@@ -745,8 +840,7 @@ void Analyzator::CreatToken()
 		}
 		if (fl)
 		{
-			TS.add_el(RSTR);
-			t.TokenValue = TS.Get_NTS();
+			t.TokenValue = TS.el_add(RSTR, Count_Line_TL);
 		}
 		RSTR = 0;
 	}
@@ -799,6 +893,8 @@ void Analyzator::CreatToken()
 		t.TokenValue = ROT;
 	TL[NTL] = t;
 	NTL++;
+	Count_TL++;
+	
 }
 void Analyzator::A1()
 {
@@ -1469,6 +1565,7 @@ void Analyzator::H1()
 }
 int main()
 {
+	setlocale(LC_ALL, ".1251");
 	Analyzator A;
-	A.Start("i.txt");
+	A.Start("i2.txt");
 }
