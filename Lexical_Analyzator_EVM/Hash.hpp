852e49d185fb6ccf7095704ff97811b0489493ba
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2025-05-11 23:42:16 +0300

    Update Hash.hpp

--- Lexical_Analyzator_EVM/Hash.hpp
+++ Lexical_Analyzator_EVM/Hash.hpp
@@ -1,216 +1,213 @@
+#pragma once 
+
+#include <iostream> 
+#include <iomanip> 
 using namespace std;
-#include <iostream>
-struct EL_TS
+struct TS_el
 {
-	int label; // значение метки
-	int ind_TL; // номер в таблице лексем
-	int next; // альтератива
+    int label;
+    int index_el;
+    int next; 
 };
+
 class Hash
 {
-	int n_max; // максимальная размерность таблицы
-	int n_os; // текущая размерность
-	int NTS = 101; // первая свободная запись в таблице строк
-	EL_TS* t;
+    int n_max;
+    int n_os; 
+    int NTS;
 
-	int Hash_function(int key)
-	{
-		return (key % n_os) + 1;
-	}
+    TS_el* t;
+
+    int hashfunc(int key) { return (key % n_os) + 1; }
+
 public:
-	int Get_NTS()
-	{
-		return NTS;
-	}
-	Hash(int n1 = 200, int os = 100)
-	{
-		n_max = n1;
-		n_os = os;
-		NTS = os + 1;
-		t = new EL_TS[n_max];
-		for (int i = 0; i < n_max; i++)
-		{
-			t[i].label = t[i].ind_TL = -1;
-			t[i].next = -1;
-		}
 
-	}
-	Hash(Hash& other)
-	{
-		n_max = other.n_max;
-		n_os = other.n_os;
-		NTS = other.NTS;
+    Hash(int n1 = 1000, int os = 100)
+    {
+        n_max = n1;
+        n_os = os;
+        NTS = os + 1;
 
-		for (int i = 0; i < n_max; i++)
-		{
-			t[i].next = other.t[i].next;
-			t[i].label = other.t[i].label;
-			t[i].ind_TL = other.t[i].ind_TL;
-		}
-	}
-	int find(int key)
-	{
-		int i = Hash_function(key);
-		if (t[i].label == key)
-			return i;
-		while (t[i].next != -1)
-		{
-			i = t[i].next;
-			if (t[i].label == key)
-				return i;
-		}
-		return -1;
-	}
-	void add_el(int key)
-	{
-		int i = Hash_function(key);
-		int fl = 0;
-		if (t[i].label == -1)
-		{
-			t[i].label = key;
-			t[i].ind_TL = i;
-			t[i].next = 0;
-		}
-		else
-		{
-			int del_ch = t[0].next;
-			int i_next = t[i].next;
-			while (t[i_next].label != -1)
-			{
-				i = i_next;
-				i_next = t[i].next;
-			}
-			if (del_ch == 0)
-			{
-				i_next = NTS;
-				t[i_next].ind_TL = NTS;
-				t[i].next = i_next;
-			}
-			else
-			{
-				i_next = del_ch;
-				t[i].next = del_ch;
-				if (t[del_ch].next != 0)
-				{
-					t[0].next = t[del_ch].next;
-				}
-				else
-				{
-					t[0].next = 0;
-				}
-				fl = 1;
-			}
-			if (fl == 0)
-			{
-				NTS++;
-			}
-			t[i_next].label = key;
-			t[i_next].next = 0;
-		}
-	}
-	void remove_el(int key)
-	{
-		//i = который дуаляем i_prev - перед i
-		int i = Hash_function(key);
-		if (t[i].label == key)
-		{
-			if ((t[i].next != 0) || (t[i].next == 0 && t[i].ind_TL >= 101))
-			{
-				if (t[0].next == 0)
-				{
-					t[0].next = t[i].next;
-				}
-				else
-				{
-					//добавляем ind_TL в удаляемые индексы
-					int g = t[0].next;
-					while (t[g].next != 0)
-					{
-						g = t[g].next;
-					}
-					t[g].next = t[i].next;
-				}
-				int i_next = t[i].next;
-				t[i].label = t[i_next].label;
-				t[i].next = t[i_next].next;
-				t[i_next].next = 0;
-			}
-			else
-			{
-				t[i].label = -1;
-				t[i].ind_TL = -1;
-				t[i].next = 0;
-			}
-		}
-		else
-		{
-			int i_prev;
-			while (t[i].label != key)
-			{
-				i_prev = i;
-				i = t[i].next;
+        t = new TS_el[n_max];
 
-			}
-			if (t[0].next == 0)
-			{
-				t[0].next = t[i].next;
-			}
-			else
-			{
-				//добавляем ind_TL в удаляемые индексы
-				int g = t[0].next;
-				while (t[g].next != 0)
-				{
-					g = t[g].next;
-				}
-				t[g].next = t[i].ind_TL;
-			}
-			t[i_prev].next = t[i].next;
-			t[i].next = 0;
+        for (int i = 0; i < n_max; i++)
+        {
+            t[i].label = t[i].index_el = -1;
+            t[i].next = 0;
+        }
+    }
+
+    Hash(const Hash& other)
+    {
+        n_max = other.n_max;
+        n_os = other.n_os;
+        NTS = other.NTS;;
+        t = new TS_el[n_max];
+
+        for (int i = 0; i < n_max; i++)
+        {
+            t = other.t;
+        }
+    }
+
+    Hash(Hash& other)
+    {
+        n_max = other.n_max;
+        n_os = other.n_os;
+        NTS = other.NTS;;
+        t = new TS_el[n_max];
+
+        for (int i = 0; i < n_max; i++)
+        {
+            t = other.t;
+        }
+    }
+
+    ~Hash()
+    {
+        delete[] t;
+    }
+    int Get_NTS()
+    {
+        return NTS;
+    }
+    int Find(int key) 
+    {
+        int i = hashfunc(key);
+
+        while (t[i].label != key)
+        {
+            if (t[i].next != 0)
+                i = t[i].next;
+            else
+                return 0;
+        }
+        return i;
+    }
+
+    int TLinf(int i)
+    {
+        if (i >= n_max)
+            return -1;
+        return t[i].index_el;
+    }
+
+    void el_del(int key)
+    {
 
 
-		}
-	}
-	void print_Hash()
-	{
-		int i = 0;
-		while (i < 101)
-		{
-			if (t[i].label != -1)
-			{
-				int g = i;
-				int fl = 1;
-				while (fl)
-				{
-					if (t[g].next != 0)
-					{
-						cout << "(" << t[g].label << "," << t[g].ind_TL << ")" << "->";
-						g = t[g].next;
-					}
-					else
-					{
-						cout << "(" << t[g].label << "," << t[g].ind_TL << ")" << endl;
-						fl = 0;
-						i++;
-					}
-				}
-				fl = 1;
-			}
-			else
-				i++;
-		}
-		cout << "\n\n";
-		cout << "t[0]:";
-		int j = 0;
-		while (t[j].next != 0)
-		{
-			if (t[j].next != 0)
-			{
-				cout << "(" << t[j].next << ")" << "->";
-				j = t[j].next;
-			}
-		}
-		cout << "(" << t[j].next << ")" << endl;
-	}
+        int k = hashfunc(key);
+        if (t[k].label == key && t[k].next == 0) 
+        {
+            t[k].label = -1;
+            t[k].index_el = -1;
+            return;
+        }
 
+
+        if (t[k].label == key && t[k].next != 0) 
+        {
+            int p = k;//
+            int sv = t[k].next;
+            while (t[t[p].next].next != 0) 
+            {
+                p = t[p].next;
+            }
+            t[k] = t[t[p].next]; 
+            t[k].next = sv;
+            t[t[p].next].next = 0;
+            t[t[p].next].index_el = -1;
+            t[t[p].next].label = -1;
+            if (t[0].next == 0)
+                t[0].next = t[p].next; 
+            else
+            {
+                int cpy = t[0].next;
+                t[0].next = t[p].next;
+                t[t[p].next].next = cpy;
+            }
+            t[p].next = 0;
+
+            return;
+        }
+
+
+
+        while (t[t[k].next].label != key)    
+            k = t[k].next;
+
+        int cpy = t[k].next; 
+        t[k].next = t[cpy].next; 
+        t[cpy].label = -1;
+        t[cpy].index_el = -1; 
+        if (t[0].next == 0) 
+        {
+            t[0].next = cpy;
+            t[cpy].next = 0;
+        }
+        else
+        {   
+            int cpy1 = t[0].next;
+            t[0].next = cpy;
+            t[cpy].next = cpy1;
+        }
+        return;
+    }
+
+    int el_add(int key, int ind) 
+    {
+        int k = hashfunc(key);
+        if (t[k].label == -1)
+        {
+            t[k].label = key;
+            t[k].index_el = ind;
+            return k;
+        }
+
+        int p = k;
+        while (t[p].next != 0) 
+            p = t[p].next;
+
+
+
+        if (t[0].next == 0)
+        {
+            t[p].next = NTS;
+            t[NTS].label = key;
+            t[NTS].index_el = ind;
+            NTS++;
+            return NTS - 1;
+        }
+
+        if (t[0].next != 0)
+        {
+            t[p].next = t[0].next;
+            int sv = t[0].next; 
+
+
+            t[0].next = t[sv].next; 
+            t[sv].label = key;
+            t[sv].index_el = ind;
+            t[sv].next = 0;
+            return sv;
+        }
+
+    }
+
+    void print()
+    {
+        std::cout << std::setw(7) << 0 << "|"
+            << std::setw(10) << t[0].label << "|"
+            << std::setw(10) << t[0].index_el << "|"
+            << std::setw(10) << t[0].next << "|" << std::endl;
+        for (int i = 1; i < n_max; i++)
+        {
+            if (t[i].label != -1)
+                std::cout << std::setw(7) << i << "|"
+                << std::setw(10) << t[i].label << "|"
+                << std::setw(10) << t[i].index_el << "|"
+                << std::setw(10) << t[i].next << "|" << std::endl;
+        }
+        std::cout << std::endl;
+    }
 };
